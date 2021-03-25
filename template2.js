91113197e899afeb7af3944e993a04e35041f56f
aaronflcc <36057802+aaronflcc@users.noreply.github.com>
2021-03-25 11:37:40 -0400

    Some modernizing updates

--- template2.js
+++ template2.js
@@ -1,11 +1,17 @@
 //If you include this javaScript file from a script element
 //at the end of the body element...
 //You can set up references to page elements here like this:
-const textInput1 = document.getElementById("text1");
-const textInput2 = document.querySelector("#text2");
+const numberInput = document.getElementById("number1");
+const textInput = document.querySelector("#text2");
 
+//Find a button and decide which function it should invoke
+document.getElementById("action1").addEventListener("click", action1);
+document.getElementById("action2").addEventListener("click", action2);
+
+//Rename these functions to suit your needs
+//right-click and choose "Rename Symbol" so it updates throughout code
 function action1() {
-    console.log("action1 called");
+    console.log(`action1 called. Inputs: ${numberInput.value}, ${textInput.value}`);
 }
 
 function action2() {
@@ -14,8 +20,8 @@ function action2() {
     //Example code: store text fields, and change the
     //content of every paragraph in the second panel
     //to a combination (concatenation) of text strings
-    let str = textInput1.value;
-    let str2 = textInput2.value;
+    let str = numberInput.value;
+    let str2 = textInput.value;
     let paragraphs = document.querySelectorAll("#secondPanel p");
     console.log(paragraphs.length + " paragraphs found.");
     for (let p of paragraphs) {
